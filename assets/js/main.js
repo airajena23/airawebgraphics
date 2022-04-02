16882329f36c8ce8579564b77fcf2fdffbe6b330
Aira <airasamrat07@gmail.com>
2022-04-02 22:42:04 +0530

    i

--- assets/js/main.js
+++ assets/js/main.js
@@ -195,7 +195,7 @@ themeButton.addEventListener("click", () => {
 
 // Typing Animation using Typed JS
 var typed = new Typed(".type", {
-  strings: ["a Web", "an Android", "a Blockchain"],
+  strings: ["a Graphic", "an Web", "a UI/UX"],
   smartBackspace: true,
   startDelay: 1000,
   typeSpeed: 130,
